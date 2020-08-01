871278da6229a8ee7dec819dc399055f6c1fed61
takeru89 <t.sada1989@gmail.com>
2020-08-01 12:13:29 +0900

    itemsコントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :category, :condition, :shipping_payer, :shipping_from, :shipping_date, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
