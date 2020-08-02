b2585f2362ce482e5daa431c77c6d40e1265f546
takeru89 <t.sada1989@gmail.com>
2020-08-02 10:16:25 +0900

    view・js編集

---
+++ app/javascript/price.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function() {
+  const priceInput = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price"); 
+  const profit = document.getElementById("profit");
+
+  const priceReg = /^([1-9]\d*|0)$/;
+  const message = "半角数字のみ入力可能";
+  
+  addTaxPrice.innerHTML = message;
+  profit.innerHTML = message;
+
+  priceInput.addEventListener("input", (function() {
+    priceInt = priceInput.value;
+    if (priceInt.match(priceReg)) {
+      addTaxPrice.innerHTML = Math.floor(priceInt * 0.1);
+      profit.innerHTML = Math.floor(priceInt * 0.9);
+    } else {
+      addTaxPrice.innerHTML = message;
+      profit.innerHTML = message;
+    }  
+  }));
+})
